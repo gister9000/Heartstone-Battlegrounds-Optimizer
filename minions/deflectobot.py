703eae0320f43286a4a9109edc03aaaca7ed736b
gister9000 <fjelic96@gmail.com>
2024-08-14 15:07:45 +0200

    feat: Deflectobot

---
+++ minions/deflectobot.py
@@ -0,0 +1,13 @@
+from minions.minion import Minion
+
+
+class DeflectOBot(Minion):
+    def __init__(self):
+        super().__init__(health=6, attack=3, divine_shield=True)
+
+    def on_minion_summon(self):
+        if not self.divine_shield:
+            print(f"{self.__class__.__name__} regained Divine Shield")
+            self.divine_shield = True
+        self.attack += 2
+        print(f"{self.__class__.__name__} gained +2 attack")
